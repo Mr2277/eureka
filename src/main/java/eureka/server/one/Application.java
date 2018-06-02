71a7c45f27bbcc74733e23e66f1e633d9cab7022
Mr2277 <837544713@qq.com>
2018-06-02 13:46:01 +0800

    init

---
+++ src/main/java/eureka/server/one/Application.java
@@ -0,0 +1,13 @@
+package eureka.server.one;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//import static org.springframework.boot.WebApplicationType.SERVLET;
+
+//@EnableEurekaServer
+public class Application {
+    public static void main(String[] args){
+      //  new SpringApplicationBuilder(Application.class).web(SERVLET).run(args);
+    }
+}
