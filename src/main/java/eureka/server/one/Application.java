c0e4d658336452a95d710cf5f277183f37a28d11
Mr2277 <837544713@qq.com>
2018-06-03 18:23:23 +0800

    init

--- src/main/java/eureka/server/one/Application.java
+++ src/main/java/eureka/server/one/Application.java
@@ -1,13 +1,14 @@
 package eureka.server.one;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 //import static org.springframework.boot.WebApplicationType.SERVLET;
 
-//@EnableEurekaServer
+@EnableEurekaServer
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args){
-      //  new SpringApplicationBuilder(Application.class).web(SERVLET).run(args);
+    public static void main(String[] args){ new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 }
